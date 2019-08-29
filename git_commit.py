c70047f8f3113565d7718d775bac2e2083cdc093
Alex Rocha <arocha@zalando-61672.corp.ad.zalando.net>
2019-08-29 16:32:00 +0200

    feat: trivial usage

--- git_commit.py
+++ git_commit.py
@@ -1,4 +1,4 @@
-#!/usr/bin/env python
+#!/usr/bin/env python3
 import subprocess
 import re
 import sys
@@ -7,37 +7,42 @@ import sys
 def print_error(func):
     def wrapper(*args, **kwargs):
         try:
-            func(*args, **kwargs)
+            return func(*args, **kwargs)
         except Exception as e:
-            print("Exception - {}".format(e.message))
+            print("Exception - {}".format(e))
             sys.exit(1)
     return wrapper
 
 
 @print_error
 def fetch_branch_name():
-    return str(subprocess.run(["git", "rev-parse", "--abbrev-ref", "HEAD"], stdout=subprocess.PIPE).stdout, 'utf-8')
+    branch_name = subprocess.run(["git", "rev-parse", "--abbrev-ref", "HEAD"], stdout=subprocess.PIPE).stdout
+    if branch_name:
+        return str(branch_name.rstrip(), 'utf-8')
+    return ""
 
 
 @print_error
 def extract_ticket_number(branch_name):
     matches = re.findall('((?:GB|gb)-\\d{4})', branch_name)
-    print(matches)
     if matches:
-        return ""
-    else:
         return matches[0]
+    else:
+        return ""
 
 
 @print_error
 def commit(msg):
-    print(msg)
-    #subprocess.run(['git', 'commit', '-m', msg], stdout=subprocess.PIPE)
+    subprocess.run(['git', 'commit', '-m', msg], stdout=subprocess.PIPE)
 
 
 if __name__ == '__main__':
     commit_msg = sys.argv[1]
     ticket_number = extract_ticket_number(fetch_branch_name())
-    commit("{} - {}".format(ticket_number, commit_msg))
+    if ticket_number:
+        commit("{} - {}".format(ticket_number, commit_msg))
+    else:
+        commit("{}".format(commit_msg))
+
 
 
